7ac40e3bcc319667a9b6c976d980d9ce70b43f69
Ruwaifa Anwar <ruwaifa@Ruwaifas-MacBook-Pro.local>
2016-12-01 19:00:44 -0500

    after demo

--- examples/flask/app/views.py
+++ examples/flask/app/views.py
@@ -8,24 +8,24 @@ from models import User
 import sys
 import requests
 from threading import *
+import time
 # Facebook app details
 FB_APP_ID = '1335682093131967'
 FB_APP_NAME = 'testing'
 FB_APP_SECRET = '7cc4885f7b114f87747bb664d6a3a200'
 mytoken=""
 
-def print_me(string):
-    print(string, file=sys.stderr)
 
 
 def get_accounts():
+    """
+    Fetches all pages for the given user
+    """
     graph = facebook.GraphAPI(mytoken)
     pages = graph.get_object('me/accounts')
-    #pprint (pages['data'])
     pages_info=[]
     for page in pages['data']:
         pages_info.append(   (  page['name'], page['access_token'] )  )
-
     return pages_info
 
 
@@ -34,17 +34,26 @@ def get_results_from_api():
     pages = graph.get_object('me/accounts')
 
 def get_views(token, id, views_dict):
+    """
+    Fetches number of views from a given post ID
+    """
     graph = facebook.GraphAPI(token)
     post = graph.get_connections(id, 'insights/post_impressions')
     views_dict[id] = post['data'][0]['values'][0]['value']
 
 
 def write_post(id, args):
+    """
+    Write a post to the page with specified arguments 
+    """
     graph = facebook.GraphAPI(id) 
     graph.put_object(parent_object='me', connection_name = 'feed', **args)
 
 
 def get_posts_from_graph_object(obj):
+    """
+    Using 'next' field, gets next set of posts
+    """
     posts = obj['data']
     next_page = "None"
     if "paging" in obj:
@@ -54,6 +63,9 @@ def get_posts_from_graph_object(obj):
 
 @app.route('/show', methods=['POST', 'GET'])
 def show_pages():
+    """
+    Fetches published or unpublished posts and displays them
+    """
     page_id=request.args['page_id']
 
     if str(request.args['r_type'])=="Create":
@@ -67,10 +79,7 @@ def show_pages():
         res = graph.get_connections(pages['id'], 'promotable_posts', is_published=is_published, limit=8)
     else:
         res = requests.request("GET",request.args['next']).json()
-
     posts,next_page = get_posts_from_graph_object(res)
-
-    
     views_dict={}
     thread_list = []
     message_dict={} 
@@ -86,7 +95,6 @@ def show_pages():
 
     for thread in thread_list:
         thread.join()
-
     posts_info=[]
     for i in id_list:
         posts_info.append(  (message_dict[i], views_dict[i] ) )
@@ -96,22 +104,39 @@ def show_pages():
 
 @app.route('/write', methods=['POST', 'GET'])
 def write_page():
+    """
+    Recieves text from HTML form and makes POST api call to create a new page post 
+    """
+    #1480631468
     page_id = request.args['page_id']
+    post_time = request.args['time']
+    #bday=2016-11-10
     args={}
+    if post_time is not None:
+        pattern = '%Y-%m-%d'
+        epoch_time=int(time.mktime(time.strptime(post_time, pattern)))
+        print_me(epoch_time)
+     
+        args['scheduled_publish_time']=epoch_time
+
     args['message'] = request.args['text']
     args['published']= request.args['p_type'] == 'Published'
+
+    """
+    demo purposes"
+    """
+    #args['scheduled_publish_time']="1480631468"
+    #args['published']="0"
     write_post(page_id, args)
     return render_template('write_done.html')
 
 
-
-
-
 @app.route('/pages')
 def main_page():
-
+    """
+    Main page that displays the pages owned/administered by the user
+    """
     pages=get_accounts()
-    #print (pages, file=sys.stderr)
     return render_template('disp.html',pages=pages)
 
 
@@ -192,3 +217,9 @@ def get_current_user():
     # Commit changes to the database and set the user as a global g.user
     db.session.commit()
     g.user = session.get('user', None)
+
+def print_me(string):
+    """
+    used of debugging
+    """
+    print(string, file=sys.stderr)
